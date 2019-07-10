fd39bc098af9bd6521266ab5097279bb9619c2f4
Nagato Kasaki <kasaki-nagato@dmm.com>
2019-07-10 14:12:33 +0900

    remove unused require

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import {render} from 'react-dom';
 import {App} from './app.jsx';
-import { setNodeUri } from 'oo7-substrate'
 require('./denominations')
 
 render(<App/>, document.getElementById('app'));
